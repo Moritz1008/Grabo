35f3af9e1ba4d5d420e8ab036eb18e7ea5833e24
Moritz Riedel <mr-213796@rwu.de>
2023-06-21 12:36:28 +0200

    Added a flexbox for the short link and the buttons in utlItem.

--- src/components/UrlItem.vue
+++ src/components/UrlItem.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <a :href="'http://localhost:5000/' + url.shortUrl" target="_blank">http://localhost:5000/{{ url.shortUrl }}
-    </a>
-    <button>Copy link</button>
-    <button>Add to Favorites</button>
-    <p>{{ truncateUrl(url.originalUrl) }}</p>
+    <div class="url-container">
+      <a :href="'http://localhost:5000/' + url.shortUrl" target="_blank">http://localhost:5000/{{ url.shortUrl }}
+      </a>
+      <button class="right-btn">Copy link</button>
+      <button class="right-btn">Add to Favorites</button>
+    </div>
+    <p>Original Link: [{{ truncateUrl(url.originalUrl) }}]</p>
   </div>
 </template>
   
@@ -17,15 +19,27 @@ export default {
     truncateUrl(url) {
       return url.length > 40 ? url.substring(0, 40) + '...' : url;
     },
-}
+  }
 };
 </script>
 
-<style>
+<style scoped>
 button {
   margin: 1rem;
 }
+
 p {
   color: gray;
+  display: inline;
+}
+
+.url-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.url-container .right-btn {
+  margin-left: auto;
 }
 </style>
