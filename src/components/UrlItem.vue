6c217ba49e7b92d62e7bb592d1ecb2d52f8cf41c
Alex <as-213803@rwu.de>
2023-06-22 13:39:19 +0200

    copy method

--- src/components/UrlItem.vue
+++ src/components/UrlItem.vue
@@ -1,25 +1,32 @@
 <template>
   <div>
     <div class="url-container">
-      <a :href="'http://localhost:5000/' + url.shortUrl" target="_blank">http://localhost:5000/{{ url.shortUrl }}
+      <a :href="'http://localhost:5000/' + url.shortUrl" target="_blank"
+        >http://localhost:5000/{{ url.shortUrl }}
       </a>
-      <button class="right-btn">Copy link</button>
+      <button class="right-btn" @click="copyUrl">Copy link</button>
       <button class="right-btn">Add to Favorites</button>
     </div>
     <p>Original Link: [{{ truncateUrl(url.originalUrl) }}]</p>
   </div>
 </template>
-  
+
 <script>
 export default {
-  props: ['url'],
+  props: ["url"],
 
   methods: {
     // limits the displayed length to 40, created with chatgpt
     truncateUrl(url) {
-      return url.length > 40 ? url.substring(0, 40) + '...' : url;
+      return url.length > 40 ? url.substring(0, 40) + "..." : url;
     },
-  }
+
+    copyUrl() {
+      navigator.clipboard.writeText(
+        "http://localhost:5000/" + this.url.shortUrl
+      );
+    },
+  },
 };
 </script>
 
